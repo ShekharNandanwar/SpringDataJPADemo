09b6cf79141739fe4df802efa0fba63a49f0167a
chandrashekharn <chandrashekhar.nandanwar@itariumtech.com-at-234333030492>
2023-01-23 18:00:23 +0530

    Testing Repository layer with different JPA queries

---
+++ src/main/java/com/shekhar/SpringDataJPADemo/entity/Guardian.java
@@ -0,0 +1,34 @@
+package com.shekhar.SpringDataJPADemo.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "name",
+                column = @Column(name = "guardian_name")
+        ),
+        @AttributeOverride(
+                name = "email",
+                column = @Column(name = "guardian_email")
+        ),
+        @AttributeOverride(
+                name = "mobile",
+                column = @Column(name = "guardian_mobile")
+        )
+})
+public class Guardian {
+    private String name;
+    private String email;
+    private String mobile;
+}
---
+++ src/main/java/com/shekhar/SpringDataJPADemo/repository/StudentRepository.java
@@ -0,0 +1,35 @@
+package com.shekhar.SpringDataJPADemo.repository;
+
+import com.shekhar.SpringDataJPADemo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByFirstName(String studentName);
+    List<Student> findByFirstNameContains(String name);
+    List<Student> findByFirstNameIgnoreCaseContaining(String name);
+    List<Student> findByLastNameNotNull();
+
+    //JPQL
+    @Query("select s from Student s where s.emailId = ?1")
+    Student getStudentByEmailAddress(String emailId);
+
+    //Native Query
+    @Query(
+            value = "select * from tbl_student s where s.email_address = ?1",
+            nativeQuery = true
+    )
+    Student getStudentByEmail(String emailId);
+
+    //Native named parameter query
+   /* @Query(
+            value = "select student_id, first_name, last_name from tbl_student s where s.email_address = :emailId",
+            nativeQuery = true
+    )
+    List<Student> getStudentNameByEmail(@Param("emailId") String emailId);*/
+}
